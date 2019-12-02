e8f23c69c140bf4d2824099bc85428d0822daafb
Isaev Aleksey <isaev.alex75311@gmail.com>
2019-12-02 23:20:47 +0300

    Task 27 - Accepted

--- 27.py
+++ 27.py
@@ -1,24 +1,22 @@
 w, h = map(int, input().split())
 
-max = w * h
-res = [0] * w
+m = w * h
+res = []
 for i in range(w):
-    res[i] = [0] * h
+    res.append([0] * h)
+    # print(res[i])
 
 
 def zap(s):
     *mass, = map(int, s.split())
     for i in range(mass[0], mass[2]):
-        for j in range(mass[1], mass[3]):
-            if res[i][j] == 1:
-                continue
-            else:
-                res[i][j] = 1
+        # for j in range(mass[1], mass[3]):
+        res[i][mass[1]:mass[3]] = [1] * (mass[3] - mass[1])
 
 
 for _ in range(int(input())):
     zap(input())
 
 for i in range(len(res)):
-    max -= res[i].count(1)
-print(max)
+    m -= res[i].count(1)
+print(m)
